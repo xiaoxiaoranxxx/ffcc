3f32c5f546fce5e1bfaec5da16170953166255b4
xiaoxiaoran <2425940130@qq.com>
2022-02-15 17:07:30 +0800

    gnore

--- src/index.js
+++ src/index.js
@@ -4,5 +4,10 @@ import ReactDom from "react-dom";
 // const title = React.createElement('h1', null, 'xiaoxiaoran')
 // ReactDom.render(title, document.getElementById('root'))
 
-const title = <h1>JSX</h1>
-ReactDom.render(title,document.getElementById('root'))
+const name = "xiaoxiaoran"
+const age = 11
+
+const title = (<h1 className="title" >JSX,name={name},age={age}<span /></h1>)
+
+
+ReactDom.render(title, document.getElementById('root'))
