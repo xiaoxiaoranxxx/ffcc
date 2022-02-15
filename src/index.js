782264bfcefbb961d107538850a7853447d5bd76
xiaoxiaoran <2425940130@qq.com>
2022-02-15 16:57:14 +0800

    初始化

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+// const title = React.createElement('h1', null, 'xiaoxiaoran')
+// ReactDom.render(title, document.getElementById('root'))
+
+const title = <h1>JSX</h1>
+ReactDom.render(title,document.getElementById('root'))
